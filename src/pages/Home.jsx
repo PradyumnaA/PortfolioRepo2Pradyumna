25b411ae14b6cf395fb7c4b8f71a39a6e3e4ef1f
PradyumnaAnilKumarKubear1998 <p_anilkumarkubear@u.pacific.edu>
2024-01-07 21:51:15 -0800

    SKy Completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,57 +1,9 @@
-// import React, { Suspense } from 'react'
-// import { Canvas } from '@react-three/fiber'
-// import Loader from '../components/Loader'
-// import {Island} from '../models/Island'
-// {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-// POPUP
-// </div> */}
-// const Home = () => {
-//   const adjustIslandForScreenSize=()=>{
-
-//     let screenScale=null
-//     let screenPosition=[0,-6.5,-43];
-//     let rotation=[0.1,4.7,0]
-//     if(window.innerWidth<768){
-//       screenScale=[0.9,0.9,0.9]
-      
-//     }
-//     else{
-//       screenScale=[1,1,1]
-
-//     }
-//     return [screenScale,screenPosition,rotation]
-//   }
-//   const [islandScale,islandPosition,islandRotation]=adjustIslandForScreenSize
-//   return (
-//     <section className='w-full h-screen relative'>
-//             {/* 3D Objects and Lights will be rendered here */}
-//         <Canvas className='w-full h-screen bg-transparent'
-//         camera={{near:0.1,far:1000}}
-//         >
-//             <Suspense fallback={<Loader/>}>
-//                 <directionalLight/>
-//                 <ambientLight/>
-//                 <pointLight/>
-//                 <spotLight/>
-//                 <hemisphereLight/>
-//                 <Island
-//                 position={islandPosition}
-//                 scale={islandScale}
-//                 rotation={islandRotation}
-//                 />
-
-//             </Suspense>
-//         </Canvas>
-//     </section>
-//   )
-// }
-
-// export default Home
 import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 import {Island} from '../models/Island'
-
+// import Sky from '../models/Sky'
+import Sky from '../models/Sky'
 const Home = () => {
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
@@ -80,11 +32,13 @@ const Home = () => {
           <spotLight />
           <hemisphereLight skycolor="#b1e1ff" groundColor="#000000"
           intensity={0.0}/>
+          <Sky/>
           <Island
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
           />
+
         </Suspense>
       </Canvas>
     </section>
