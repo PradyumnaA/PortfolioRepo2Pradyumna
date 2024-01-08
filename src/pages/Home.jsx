417e941d15d3adffb94b72b0de9dd723fc706c3a
PradyumnaAnilKumarKubear1998 <p_anilkumarkubear@u.pacific.edu>
2024-01-08 13:25:13 -0800

    Person grabbing working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
-import React, { Suspense } from 'react'
+import React, {useState, Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 import {Island} from '../models/Island'
-// import Sky from '../models/Sky'
+
 import Sky from '../models/Sky'
 import Bird from '../models/Bird'
 import Plane  from '../models/Plane'
 const Home = () => {
+  const[isRotating,setIsRotating]=useState(false)
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let screenPosition = [0, -6.5, -43];
@@ -26,7 +27,14 @@ const Home = () => {
 // 48.22
   return (
     <section className='w-full h-screen relative'>
-      <Canvas className='w-full h-screen bg-transparent' camera={{ near: 0.1, far: 1000 }}>
+      <Canvas 
+      className={
+        `w-full h-screen bg-transparent ${isRotating ? 
+        'cursor-grabbing':'cursor-grab'}
+        camera={{near:0.1,far:1000}}`
+      }
+      
+      >
         <Suspense fallback={<Loader />}>
           
           <directionalLight position={[1,1,1]} intensity={2}/>
