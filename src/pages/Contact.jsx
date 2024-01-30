2f2d8c5385a4ea1295619e392c58ad0adbf3c234
PradyumnaAnilKumarKubear1998 <p_anilkumarkubear@u.pacific.edu>
2024-01-29 21:27:04 -0800

    02:00:29 commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,8 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, Suspense } from 'react';
 import emailjs from '@emailjs/browser'
+import { Canvas } from '@react-three/fiber';
+import { Fox } from '../models/Fox';
+import { Loader } from '@react-three/drei';
 const Contact = () => {
   const formRef = useRef(null);
   const [form, setForm] = useState({
@@ -102,6 +105,25 @@ const Contact = () => {
             {isLoading ? 'Sending...' : 'Send Message'}
           </button>
         </form>
+      </div>
+      <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+        <Canvas
+        camera={{
+          position:[0,0,5],
+          fov:75,
+          near:0.1,
+          far:1000
+        }}
+        >
+          <directionalLight intensity={2.5} position={[0,0,1]}/>
+          <Suspense fallback={<Loader/>}>
+            <Fox
+            position={[0.5,0.5,0.5]}
+            rotation={[12.6,-0.6,0]}
+            scale={[0.5,0.5,0.5]}
+            />
+          </Suspense>
+        </Canvas>
       </div>
     </section>
   );
